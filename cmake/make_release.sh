86cb385154b9b79b520c1573fb4c5a0f380f68e1
Christoph Junghans <junghans@lanl.gov>
2021-02-06 16:32:25 -0700

    add make_release script

---
+++ cmake/make_release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+
+die () {
+  echo -e "$*"
+  exit 1
+}
+
+[[ $1 ]] || die "Usage: ${0} <version>"
+[[ $1 = 1.[0-9].[0-9] ]] || die "Version has the wrong format"
+
+sed -i "s/VERSION 1.[0-9].[0-9]/VERSION $1/" CMakeLists.txt
+sed -i "/PROJECT_NUMBER         =/s/=.*/= $1/" doc/doxygen/Doxyfile
+sed -i "1s/Libecpint.*/Libecpint $1/" README.md
+
+git add -u
+git commit -m "Bump version to $1"
+git tag v"$1"
